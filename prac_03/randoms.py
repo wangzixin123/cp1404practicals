f3f06e01f634459a201bcc51cfde53fe81fd65f1
22354 <zixin.wang@my.jcu.edu.au>
2023-07-25 13:47:30 +0800

    prac_03

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+
+# q1
+# print a random integer between 5 and 20
+# the lowest number could seen is 5,and the largest is 20
+# q2
+# print an odd integer between 3 and 10
+# the lowest number could see is 3,and the largest is 9
+# 4 can not been produce in line 2
+# q3
+# print a random float between 2.5 and 5.5
+# the lowest number could see is 2.5,and the largest is 5.5
+# q4
+print(random.randint(1, 100))

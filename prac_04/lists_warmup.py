6f95c979b240f6b620ae0e99e7e0fe01b2a9d53e
22354 <zixin.wang@my.jcu.edu.au>
2023-08-05 15:37:56 +0800

    prac_04

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,15 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# 3
+# 2
+# 1
+# [3,1,4,1,5,9]
+# [1]
+# ture
+# false
+# false
+# [3,1,4,1,5,9,2,6,5,3]
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[2:])
+print(9 in numbers)
+print(numbers)
